3b00296e97c814c2fa48594f3a748d8223ed5925
valmsmith39a <valmsmith39a@gmail.com>
2016-05-12 22:46:26 -0700

    implements renderRecords() in displayRecords.js component

--- public/lib/components/main/displayRecords.js
+++ public/lib/components/main/displayRecords.js
@@ -4,9 +4,26 @@ import { getAllRecords } from '../../actions/RecordActions/index';
 
 
 class DisplayRecords extends React.Component {
+  constructor(props) {
+    super(props)
+    // this.props.getAllRecords()
+  }
+
+  renderRecords() {
+    return (
+      <ul>
+        this.state.records.map(record => {
+          return <Record record={record} index={index} />
+        })
+      </ul>
+    )
+  }
+
   render() {
     return(
-      <div>DisplayRecords</div>
+      <div>DisplayRecords
+        {this.renderRecords()}
+      </div>
     )
   }
 }
