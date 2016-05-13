4733a17c87ffecbe83112a4b8d623d96fdbb0927
valmsmith39a <valmsmith39a@gmail.com>
2016-05-12 23:57:15 -0700

    implements EditRecord component

--- public/lib/components/main/editRecord.js
+++ public/lib/components/main/editRecord.js
@@ -3,10 +3,39 @@ import { connect } from 'react-redux';
 import { editRecord } from '../../actions/recordActions';
 
 class EditRecord extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {show: false}
+  }
+
+  openEditInput() {
+    this.setState({show: true})
+  }
+
+  closeEditInput() {
+    this.setState({show: false})
+    this.props.editRecord()
+    .then(response => {
+      console.log('response: ', response);
+    })
+  }
+
+  renderEditInput() {
+    if(this.state.show) {
+      return (
+        <input />
+      )
+    } else {
+      return (<div><div>)
+    }
+  }
 
   render() {
     return(
-      <button>Edit</button>
+      <div>
+        <button onClick={this.openEditInput.bind(this)}>Edit</button>
+        {renderEditInput()}
+      </div>
     )
   }
 }
