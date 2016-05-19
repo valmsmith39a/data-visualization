92e870db339719d207fed39e4b8c01c05a6611f8
valmsmith39a <valmsmith39a@gmail.com>
2016-05-18 18:03:18 -0700

    implements first version of like functionality using component state

--- public/lib/components/main/like.js
+++ public/lib/components/main/like.js
@@ -4,18 +4,31 @@ import { connect } from 'react-redux';
 class Like extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      likeCounter: 0
+    }
     this.likeAction = this.likeAction.bind(this)
-
   }
 
   likeAction() {
-    console.log('in like action')
+    this.setState({
+      likeCounter: this.state.likeCounter + 1
+    })
+  }
+
+  likeCounter() {
+    return(
+      <div>
+        {this.state.likeCounter}
+      </div>
+    )
   }
 
   render() {
     return(
       <div>
         <button onClick={this.likeAction}>Like</button>
+        {this.likeCounter()}
       </div>
     )
   }
