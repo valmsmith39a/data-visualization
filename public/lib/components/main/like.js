e3df8fa75c07a820d72b7139f9f61fb4e799e350
valmsmith39a <valmsmith39a@gmail.com>
2016-05-18 17:57:06 -0700

    adds like button and wires to likeAction

--- public/lib/components/main/like.js
+++ public/lib/components/main/like.js
@@ -2,10 +2,20 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 class Like extends React.Component {
+  constructor(props) {
+    super(props)
+    this.likeAction = this.likeAction.bind(this)
+
+  }
+
+  likeAction() {
+    console.log('in like action')
+  }
+
   render() {
     return(
       <div>
-        Like Component 
+        <button onClick={this.likeAction}>Like</button>
       </div>
     )
   }
