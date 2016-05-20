9f58b108cac7f124d50745ad8022377afbd50cbd
valmsmith39a <valmsmith39a@gmail.com>
2016-05-19 23:07:12 -0700

    experiments with LIKE case for reducer_like.js

--- public/lib/reducers/reducer_like.js
+++ public/lib/reducers/reducer_like.js
@@ -9,10 +9,11 @@ const INITIAL_STATE = []
 export default function (state=INITIAL_STATE, action) {
   switch(action.type) {
     case CREATE_RECORD:
-    console.log('in like reducer');
-      return state
+      return [...state, 0]
     case LIKE:
-      return state
+      let index = action.payload.index
+      let numOfLikes = state.slice(index, index + 1)[0]
+      return [...state.slice(0, index), numOfLikes, ...state.slice(index+1)]
     default:
       return state
   }
